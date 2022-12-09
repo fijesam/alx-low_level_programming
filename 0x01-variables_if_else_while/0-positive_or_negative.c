e5be345a06450fcda5f55079f43365502724e0c6
fijesam <fijesam@yahoo.com>
2022-12-09 13:06:05 -0800

    if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,16 +3,16 @@
 #include <time.h>
 
 /**
- *
  * main - main function to generate a random number
  *
  * Return: Always 0 (Success)
  *
  */
 
-int main (void)
+int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
