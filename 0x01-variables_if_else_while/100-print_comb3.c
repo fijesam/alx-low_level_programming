39513273d585fce7bf1b1acee31170cc5f36c210
fijesam <fijesam@yahoo.com>
2022-12-11 10:50:54 -0800

    testing while loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - displays all combination of two digits
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int units = '0';
+	int tens = '0';
+
+	while (tens <= '9')
+	{
+		while (units <= '9')
+		{
+			if (units != tens)
+			{
+				putchar(tens);
+		 		putchar(units);		
+			}
+			tens++;
+			units++;
+		}
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
