b0fb88317112ab50e3b01aa1f6a3efb18db5a067
fijesam <fijesam@yahoo.com>
2022-12-13 12:11:00 -0800

    forward

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	char p = 'a';
+
+	while (p <= 'z')
+	{
+		_putchar(p);
+		p++;
+	}	
+	_putchar('\n');
+}
