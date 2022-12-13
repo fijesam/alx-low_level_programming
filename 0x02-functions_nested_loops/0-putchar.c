48b738e1aaa40de3366dc72dcd036d904e0fcde0
fijesam <fijesam@yahoo.com>
2022-12-13 11:24:32 -0800

    playing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - return entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef H
+#define H
+
+int _putchar(char c)
+{
+return (write(1,&c,1));
+}
+void print _alphabet(void);
+void print _alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a,int b);
+void print_to_98(int q);
+void print_times_table(int q);
+void num(int i, int j);
+
+#endif
+
