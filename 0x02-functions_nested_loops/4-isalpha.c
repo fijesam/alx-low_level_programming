dda9c31106bb0b1f47a078da927349084a15d38a
fijesam <fijesam@yahoo.com>
2022-12-14 10:29:01 -0800

    xoxo3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for capital letters
+ *
+ * Return: 1 if capital letter, 0 if otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
