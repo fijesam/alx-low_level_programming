a2a6d0e86f758585d82076cee7dc047dc3d448fe
fijesam <fijesam@yahoo.com>
2022-12-14 06:52:08 -0800

    soo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print each alphabet 10x
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int m;
+	char n;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+	}
+	_putchar('\n');
+}
+
