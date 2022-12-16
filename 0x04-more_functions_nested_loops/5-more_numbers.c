e810cc33e35ad5ba26e0ea511622bccb62e84312
fijesam <fijesam@yahoo.com>
2022-12-16 00:59:57 -0800

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,7 +14,11 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n + 48);
+			if (n >= 10)
+			{
+				_putchar((n / 10) + 48);
+			}
+			_putchar((n % 10) + 48);
 		}
 		_putchar('\n');
 	}
