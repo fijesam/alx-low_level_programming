a2db475b07a5bc574937e707dbfc6899d584eb0c
fijesam <fijesam@yahoo.com>
2022-12-15 23:56:29 -0800

    4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - displays num
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int q = 0;
+
+	while (q < 10)
+	{
+		_putchar(q);
+		q++;
+	}
+	_putchar('\n');
+}
