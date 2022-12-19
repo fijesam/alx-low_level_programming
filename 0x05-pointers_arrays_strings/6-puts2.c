5f6662b6f6e699d5b52b29a971822330f5fa84f7
fijesam <fijesam@yahoo.com>
2022-12-19 11:50:21 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints a str
+ * @str: argument
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
