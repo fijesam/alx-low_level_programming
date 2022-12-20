412f200658b69139141dc23deb8e14ede931055d
fijesam <fijesam@yahoo.com>
2022-12-20 01:49:34 -0800

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - str to int
+ * @s: argument
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int x, a = 0;
+
+	int b, c = 1;
+
+	for (int c = 0; *(s + c) != '\0'; c++)
+	{
+		if (*(s + c) == '-')
+			b *= -1;
+		if (*(s + c) < '0' || *(s + c) > '9')
+			break;
+		if (*(s + c) >= '0' && *(s + c) <= '9')
+			c *= 10;
+	}
+	for (int i = c - x; i < c; i++)
+		a = a + ((*(s + i) - 48) * c);
+		c /= 10;
+	return (a * b);
+
+}
