3786c08b1948324694f80223c2a9ae3a72a76482
fijesam <fijesam@yahoo.com>
2022-12-21 00:31:48 -0800

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copies a str
+ * @dest: destination argument
+ * @src: source str argument
+ * @n: bytes value
+ * Return: destination str
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
