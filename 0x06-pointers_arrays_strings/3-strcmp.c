048de5d19fbc5a95ba060dc538e5e35b53871c11
fijesam <fijesam@yahoo.com>
2022-12-22 02:55:34 -0800

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,18 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	int j = 0;
-
 	int val;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	val = s1[i] - s2[j];
+	val = s1[i] - s2[i];
 	return (val);
 }
