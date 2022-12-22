7d54b06f19cdaad4d9b95ea159974c618f07acda
fijesam <fijesam@yahoo.com>
2022-12-22 03:45:11 -0800

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,17 +16,19 @@ char *cap_string(char *str)
 		if (str[0] >= 97 && str[0] <= 122)
 		{
 			str[0] = str[0] - 32;
-			break;
 		}
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.'
+		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.'
 				|| str[i] == ':' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '('
 				|| str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == '[' || str[i] == ']')
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
 				str[i + 1] = str[i + 1] - 32;
-				break;
 			}
+		}
+		else
+		{
+			break;
 		}
 	}
 	return (str);
