80be63e2c89d746e5c93a2d74b3c2c474cf34a8d
fijesam <fijesam@yahoo.com>
2022-12-22 03:23:39 -0800

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,9 +21,9 @@ char *cap_string(char *str)
 				|| str[i] == ':' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '('
 				|| str[i] == ')' || str[i] == '{' || str[i] == '}' || str[i] == '[' || str[i] == ']')
 		{
-			if (str[i++] >= 97 && str[i++] <= 122)
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
-				str[i++] = str[i++] - 32;
+				str[i + 1] = str[i + 1] - 32;
 			}
 		}
 	}
