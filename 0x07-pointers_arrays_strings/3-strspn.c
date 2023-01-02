44462bdc565288c4472118878af5b23323350cb8
fijesam <fijesam@yahoo.com>
2023-01-02 11:56:11 -0800

    commit 2

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - return thesame number of bytes from s and accept
+ * @s: string argument
+ * @accept: target argument
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	int k = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				break;
+			}
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+			{
+				return (k);
+			}
+		}
+		i++
+	}
+	return (k);
+}
