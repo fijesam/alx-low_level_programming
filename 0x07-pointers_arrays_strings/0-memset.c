b51ea25eb8745ad6957d7df36b0549b700a2b29d
fijesam <fijesam@yahoo.com>
2023-01-02 06:45:16 -0800

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * _memset - function fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * _memset - function fills the first n bytes 
+ * of the memory area pointed to by s
+ * with the constant byte b
  * @s: pointer
  * @b: constant
  * @n: bytes of memory area to fill
