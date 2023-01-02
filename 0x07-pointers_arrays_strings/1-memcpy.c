f261ef46f58ac33adaa9a80d3c5fc77696e20bad
fijesam <fijesam@yahoo.com>
2023-01-02 07:10:16 -0800

    commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,12 +14,13 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	if (size > 0)
 	{
-		int i;
+		int i = 0;
 
-		for (i = 0; i < n; i++)
+		while (i < n)
 		{
-			dest[i] = src[i];
+			i++;
 		}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
