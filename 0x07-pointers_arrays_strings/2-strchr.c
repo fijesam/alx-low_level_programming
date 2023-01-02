b4132488a91d7e576723b27754e68cfa74367308
fijesam <fijesam@yahoo.com>
2023-01-02 08:08:00 -0800

    commit 1

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * _strchr - pointer to the first occurrence c or null
+ * @s: string argument
+ * @c: character argument
+ * Return: null if c not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != c && s[i] != '\0')
+	{
+		i++;
+	}
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
